24a50e014a856192f28b7c5857c77e7372dd7daa
Hua <binhua.tein@gmail.com>
2016-09-09 12:11:26 +0800

    Add ACE tables

---
+++ C2RexDocGen.bat
@@ -0,0 +1,1 @@
+node C2RexDocGen.js
---
+++ C2RexDocGen.js
@@ -0,0 +1,33 @@
+var main = function()
+{
+    var configuration = {
+        "C2Root": "d:/Construct 2",
+        "output":"./c2rexpluginsACE",
+        "filter": {
+            "plugins": /rex_/gi,
+            "behaviors": /rex_/gi,
+        }
+    };
+
+    var startTime = (new Date()).getTime();    
+    console.log("Start...")
+    var docGen = require("../C2DocGen/docGen");    
+    var callback = function(err, result)
+    {
+        if (err)
+            console.log(err)
+        else
+        {
+            console.log("Done");
+            //var open = require('open');
+            //open(configuration["output"] + "/index.html");
+        }
+        
+        var endTime = (new Date()).getTime();
+        var elapsedTime = (endTime - startTime)/1000;
+        console.log("Elapsed time= " + elapsedTime + " seconds")
+    };
+    docGen(configuration, callback);
+};
+
+main();
