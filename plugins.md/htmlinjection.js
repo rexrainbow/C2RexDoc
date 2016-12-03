14b8c4e2c2ecd08f008b9075af80e46b7b1c92eb
Hua <binhua.tein@gmail.com>
2016-12-03 15:55:06 +0800

    Update

---
+++ plugins.md/htmlinjection.js
@@ -0,0 +1,43 @@
+var fs = require("fs");
+var async = require("async");
+
+// inject GoogleAnalytics
+var ContentGA = `
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-88123342-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+`;
+function injectGoogleAnalytics(content)
+{
+    if (content.indexOf(ContentGA) !== (-1))
+        return content;
+    
+    var start = content.indexOf("</head>");
+    content = content.slice(0, start) + ContentGA + content.slice(start, content.length);
+    return content;
+};
+// inject GoogleAnalytics
+
+
+// main
+var names = fs.readdirSync(".");
+names = names.filter(function(n)
+{
+    return (n.split(".")[1] === "html");
+});
+
+async.map(names, function(name, cb){
+    console.log(name);
+    var content = fs.readFileSync(name, 'utf8');
+    content = injectGoogleAnalytics(content);
+    fs.writeFileSync(name, content);
+}
+);
+// main
